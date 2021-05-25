802ba1f47ae95dcde34619794264a9092be14c81
Nurlan <nurlaney@code.edu.az>
2021-05-26 03:29:26 +0400

    Loader added

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -3,15 +3,21 @@ import { Product } from './Product';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToBasket, loadProducts, showBasket } from '../../redux/actions/actions';
 import './product.scss';
+import Spinner from 'react-bootstrap/Spinner';
 
 export const ProductList = () => {
     const products = useSelector(state => state.allProducts.products);
     const [shoBasket, setShoBasket] = useState(true);
     const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+
     const fetchProducts = useCallback(() => fetch('https://isko88.github.io/apipizza.json')
         .then(resp => resp.json())
         .then(products => {
             dispatch(loadProducts(products));
+        }).finally(() => {
+            setIsLoaded(true);
         }), [dispatch])
 
     const handleShowBasket = () => {
@@ -29,18 +35,27 @@ export const ProductList = () => {
 
 
     return (
-        <section id='products'>
-            <h3 className='text-center'>Products</h3>
-            <h4 onClick={handleShowBasket} className='text-right'><i className="fas fa-shopping-basket"></i></h4>
-            <div className='row'>
-                {
-                    products.map((product) => (
-                        <div className='col-md-4' key={product.id}>
-                            < Product value={product} handlePizzaId={handlePizzaId} />
+        <>
+            {
+                isLoaded ? (
+                    <section id='products' >
+                        <h3 className='text-center'>Products</h3>
+                        <h4 onClick={handleShowBasket} className='text-right'><i className="fas fa-shopping-basket"></i></h4>
+                        <div className='row'>
+                            {
+                                products.map((product) => (
+                                    <div className='col-md-4' key={product.id}>
+                                        < Product value={product} handlePizzaId={handlePizzaId} />
+                                    </div>
+                                ))
+                            }
                         </div>
-                    ))
-                }
-            </div>
-        </section>
+                    </section >
+                ) : (
+                    <Spinner animation='border' />
+                )
+            }
+        </>
+
     )
 }
